a800f853fe4128527711f6fdca4a58f976bcb586
nabachouhan <nabakkrr@gmail.com>
2026-03-28 22:41:52 +0530

    updated package.json and updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ol: ['ol', 'ol-layerswitcher'],
+          turf: ['@turf/turf'],
+          export: ['html2canvas', 'jspdf'],
+          formats: ['shpjs', '@mapbox/shp-write', '@mapbox/togeojson', '@maphubs/tokml', 'jszip'],
+          vendor: ['proj4', 'axios']
+        }
+      }
+    }
   }
-}
+});
